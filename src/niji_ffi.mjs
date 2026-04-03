b4f285e83e4d558e5ac95593acf20efc5865e969
Hannes Nevalainen <hannes.nevalainen@me.com>
2026-04-03 22:51:11 +0200

    fix atan2 in ffi

--- src/niji_ffi.mjs
+++ src/niji_ffi.mjs
@@ -4,6 +4,6 @@ export function sin(value) {
 export function cos(value) {
   return Math.cos(value);
 }
-export function atan2(value) {
-  return Math.atan2(value);
+export function atan2(y, x) {
+  return Math.atan2(y, x);
 }
